c022c561996a26e687b01fd19575e88a1af10db5
abeekuharmony <abeekuharmony@gmail.com>
2022-09-08 16:50:40 -0700

    Hello,printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,2 +1,13 @@
-#!/bin/bash
+#include <stdio.h>
 
+/**
+ * main displays with proper grammar,
+ * but the outcome is a piece of art,
+ *
+ * Return: 0 if exited properly,  otherwise display a non-zero number
+ */
+int main(void)
+{
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
+}
