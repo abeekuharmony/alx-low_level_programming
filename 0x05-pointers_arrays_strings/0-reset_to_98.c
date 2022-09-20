b4117bb6f2e7413c707279634ea143081701170a
abeekuharmony <abeekuharmony@gmail.com>
2022-09-20 16:28:58 -0700

    98 Battery st.

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -9,21 +9,3 @@ void reset_to_98(int *n)
 {
 	*n = 98;
 }
-
-task 1
-
-#include "main.h"
-
-/**
- * swap_int - swaps the values of two integers
- * @a: pointer to first value
- * @b: pointer to second value
- */
-void swap_int(int *a, int *b)
-{
-	int c;
-
-	c = *a;
-	*a = *b;
-	*b = c;
-}
